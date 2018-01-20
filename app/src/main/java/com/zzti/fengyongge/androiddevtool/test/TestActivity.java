c9c8b68892c3924a5ea31344d73de40745e42c64
fsuper <917386389@qq.com>
2018-01-20 15:28:43 +0800

    新增test1

--- app/src/main/java/com/zzti/fengyongge/androiddevtool/test/TestActivity.java
+++ app/src/main/java/com/zzti/fengyongge/androiddevtool/test/TestActivity.java
@@ -11,6 +11,6 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-        //test
+        //test11
     }
 }
